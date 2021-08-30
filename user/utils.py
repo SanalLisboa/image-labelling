b9a6b37d6b70f48b59e0742ed78afc754e31c1a9
Sanal Lisboa <sanallisboa@Sanals-MacBook-Pro.local>
2021-08-30 10:19:49 +1000

    Adding documentation to code inside user and structuring

--- user/utils.py
+++ user/utils.py
@@ -1,36 +1,29 @@
 from typing import Dict
 
-from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 
-
-class UserExistException(Exception):
-    
-    def __init__(self, message: str):
-
-        self.message = message
-        super().__init__(message)
+from user.exceptions import UserExistException
 
 
 def create_user(data: Dict[str, str]) -> User:
+    """This function is used to create a user."""
 
     try:
 
-        User.objects.get(
-            username=data['username']
-        )
+        User.objects.get(username=data["username"])
         raise UserExistException(
-            "User with username {}, already exists".format(data['username'])
+            "User with username {}, already exists".format(data["username"])
         )
 
     except User.DoesNotExist:
 
         user = User.objects.create_user(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            email=data['email'],
-            username=data['username'],
-            password=data['password'],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            email=data["email"],
+            username=data["username"],
+            password=data["password"],
         )
 
     return user
--- user/views.py
+++ user/views.py
@@ -1,33 +1,26 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from user.exceptions import UserExistException
 from user.serializers import RegisterSerializer
-from user.utils import create_user, UserExistException
+from user.utils import create_user
 
 
 class RegisterView(APIView):
+    """ "This view is used to register a user"""
 
     def post(self, request):
 
-        request_serializer = RegisterSerializer(
-            data=request.data
-        )
+        request_serializer = RegisterSerializer(data=request.data)
         request_serializer.is_valid(raise_exception=True)
 
         try:
             user = create_user(request_serializer.data)
         except UserExistException as e:
-            return Response(
-                {
-                    "error": e.message
-                }, status=status.HTTP_409_CONFLICT
-            )
+            return Response({"error": e.message}, status=status.HTTP_409_CONFLICT)
 
         return Response(
-            {
-                "message": "Registeration successfull"
-            }, status=status.HTTP_201_CREATED
+            {"message": "Registeration successfull"}, status=status.HTTP_201_CREATED
         )
- 
